7163e9cf26e758033bc4c7ff71b992a70cd54841
elpinal <elpinal@users.noreply.github.com>
2017-12-26 18:01:48 +0900

    Tweak Run

--- cli/cli.go
+++ cli/cli.go
@@ -10,8 +10,8 @@ import (
 type CLI cli.CLI
 
 func (c *CLI) Run(args []string) int {
-	if h := time.Now().Hour(); h >= 21 || h <= 5 {
-		fmt.Println("Don't be awake during 09:00 p.m. ... 05:00 a.m.")
+	if h := time.Now().Hour(); h >= 21 || h < 6 {
+		fmt.Println("Don't be awake during 09:00 p.m. .. 06:00 a.m.")
 		// This return value is an arbitrary number to distinguish from
 		// any other errors ((*cli.CLI).Run may not return it.)
 		return 3
