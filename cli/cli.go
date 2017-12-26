9fc387af7faf8286b7adb1b876ecc57ac7118670
elpinal <elpinal@users.noreply.github.com>
2017-12-26 17:51:05 +0900

    Mention the return value

--- cli/cli.go
+++ cli/cli.go
@@ -12,6 +12,8 @@ type CLI cli.CLI
 func (c *CLI) Run(args []string) int {
 	if h := time.Now().Hour(); h >= 21 || h <= 5 {
 		fmt.Println("Don't be awake during 09:00 p.m. ... 05:00 a.m.")
+		// This return value is an arbitrary number to distinguish from
+		// any other errors ((*cli.CLI).Run may not return it.)
 		return 3
 	}
 	return (*cli.CLI).Run((*cli.CLI)(c), args)
